8778da50460ee5710a44128acdf2986086ab89eb
Raymond Walintukan <raytochina@gmail.com>
2013-12-22 07:43:53 -0800

    Inserted first few models

--- medusa_backend/settings.py
+++ medusa_backend/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Models go here
+    'sop_log',
 )
 
 MIDDLEWARE_CLASSES = (
--- sop_log/models.py
+++ sop_log/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+''' Models for the Checklists '''
+
+class Checklist(models.Model):
+    name = models.CharField(max_length=30)
+    group_id = models.ForeignKey('Group')
+    #category_id = models.ForeignKey('Category')
+    #template_id = models.ForeignKey('Template')
+    
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    
+  
