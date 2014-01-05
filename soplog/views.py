67b660879436d00818f0d3e75f0a8248fd45cb8f
Darthyogurt <raytochina@gmail.com>
2014-01-05 11:48:42 -0800

    destroy

--- soplog/views.py
+++ soplog/views.py
@@ -19,9 +19,9 @@ def testPost(request):
     #a = request.GET['alpha']
     #a = request.POST
     
-    s = ""
-    for item in request.POST:
-        s += item + request.POST[item] + " | "    
+#    s = ""
+#    for item in request.POST:
+#        s += item + request.POST[item] + " | "    
     
     print "--------------------"
     print request.body
