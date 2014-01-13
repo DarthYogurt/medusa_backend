f7083d55eaff21a5e4fb60f808b885009a618d2a
Raymond Walintukan <raytochina@gmail.com>
2014-01-13 11:10:30 -0800

    going div

--- static/jquery/checklist.js
+++ static/jquery/checklist.js
@@ -7,16 +7,17 @@ stepType; //Taken from createList.html, contains all step types
 function addCreateStep(idNum){
 	
 	var stepNum = document.createElement("label");
-	stepNum.className = "col-md-1 form-control";
+	stepNum.className = "col-md-1 ";
 	stepNum.innerHTML = idNum+1;
 	
-	var stepInput = document.createElement("input");
-	stepInput.name = "name";
-	stepInput.className = "form-control";
-	stepInput.id = "newStepId"+idNum;
+	var stepName = document.createElement("input");
+	stepName.name = "name";
+	stepName.className = "col-md-4";
+	stepName.placeholder = "name of step";
+	stepName.id = "newStepId"+idNum;
 	
 	var selectType = document.createElement("select");
-	selectType.className = "form-control";
+	selectType.className = "col-md-2";
 	for (var i=0;i<stepType.length;i++)
 	{ 
 		var option = document.createElement("option");
@@ -25,20 +26,20 @@ function addCreateStep(idNum){
 	}
 	
 	var description = document.createElement("input");
-	description.className = "form-control";
+	description.placeholder = "description";
+	description.className = "col-md-4";
 	
 	
 	var formGroup = document.createElement("div");
-	formGroup.className = "form-group form-inline";
+	formGroup.className = "row";
 	formGroup.id = "groupStepId"+idNum;
 	formGroup.appendChild(stepNum);
-	formGroup.appendChild(stepInput);
+	formGroup.appendChild(stepName);
 	formGroup.appendChild(selectType);
 	formGroup.appendChild(description);
 	
 	
 	var createStepsDiv = document.getElementById("create-steps");
-	
 	createStepsDiv.appendChild(formGroup);
 };
 
