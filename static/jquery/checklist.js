a72f382bad05e9d936e5c88a25c82cf1e5142b3b
Raymond Walintukan <raytochina@gmail.com>
2014-01-13 11:50:18 -0800

    spacing correct on step adding

--- static/jquery/checklist.js
+++ static/jquery/checklist.js
@@ -7,40 +7,50 @@ stepType; //Taken from createList.html, contains all step types
 function addCreateStep(idNum){
 	
 	var stepNum = document.createElement("label");
-	stepNum.className = "col-md-1 ";
 	stepNum.innerHTML = idNum+1;
+	var stepNumDiv = document.createElement("div");
+	stepNumDiv.className = "col-sm-1";
+	stepNumDiv.appendChild(stepNum);
 	
 	var stepName = document.createElement("input");
 	stepName.name = "name";
-	stepName.className = "col-md-4";
+	stepName.className = "form-control";
 	stepName.placeholder = "name of step";
 	stepName.id = "newStepId"+idNum;
+	var stepNameDiv = document.createElement("div");
+	stepNameDiv.className = "col-sm-4";
+	stepNameDiv.appendChild(stepName);
 	
 	var selectType = document.createElement("select");
-	selectType.className = "col-md-2";
+	selectType.className = "form-control";
 	for (var i=0;i<stepType.length;i++)
 	{ 
 		var option = document.createElement("option");
 		option.text = stepType[i];
 		selectType.appendChild(option);
 	}
+	var selectTypeDiv = document.createElement("div");
+	selectTypeDiv.className = "col-sm-2";
+	selectTypeDiv.appendChild(selectType);
 	
 	var description = document.createElement("input");
 	description.placeholder = "description";
-	description.className = "col-md-4";
+	description.className = "form-control";
+	var descriptionDiv = document.createElement("div");
+	descriptionDiv.className = "col-sm-5";
+	descriptionDiv.appendChild(description);
 	
-	
-	var formGroup = document.createElement("div");
-	formGroup.className = "row";
-	formGroup.id = "groupStepId"+idNum;
-	formGroup.appendChild(stepNum);
-	formGroup.appendChild(stepName);
-	formGroup.appendChild(selectType);
-	formGroup.appendChild(description);
+	var divRow = document.createElement("div");
+	divRow.className = "row form-group";
+	divRow.id = "groupStepId"+idNum;
+	divRow.appendChild(stepNumDiv);
+	divRow.appendChild(stepNameDiv);
+	divRow.appendChild(selectTypeDiv);
+	divRow.appendChild(descriptionDiv);
 	
 	
 	var createStepsDiv = document.getElementById("create-steps");
-	createStepsDiv.appendChild(formGroup);
+	createStepsDiv.appendChild(divRow);
 };
 
 function deleteStep(){
