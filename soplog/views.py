785b0e0914ad6f6194d2b0bb2d249d6837982b74
Raymond Walintukan <raytochina@gmail.com>
2014-02-01 20:39:07 -0800

    changed extraImage to accept into database

--- soplog/views.py
+++ soplog/views.py
@@ -137,6 +137,7 @@ def upload(request):
                                   startTime = datetime.datetime.strptime(row['timeStarted'], '%m-%d-%y %H:%M:%S' ),
                                   endTime = datetime.datetime.strptime(row['timeFinished'], '%m-%d-%y %H:%M:%S' ),
                                   addText = row.get('extraNote',""),
+                                  addImage = row.get(request.FILES[row['extraImage']],"")
                                   )
             newNumber.save()
         elif row['stepType'] == "text":
